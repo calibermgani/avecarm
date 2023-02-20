a1be89d48b997354e878b9ab79add9f3e9d01bb2
sathishcaliber <sathish@caliberfocus.com>
2023-02-20 16:24:25 +0530

    ARM-82 created fields successfully

--- backend/database/migrations/2022_12_14_124952_alter_table_import_fields_add_column_closed_claim_date.php
+++ backend/database/migrations/2022_12_14_124952_alter_table_import_fields_add_column_closed_claim_date.php
@@ -15,6 +15,8 @@ class AlterTableImportFieldsAddColumnClosedClaimDate extends Migration
     {
         Schema::table('import_fields', function (Blueprint $table) {
             $table->date('closed_claim_date')->nullable()->after('claim_Status');
+            $table->string('billed_submit_date',25)->nullable()->after('closed_claim_date');
+            $table->string('denial_code')->nullable()->after('billed_submit_date');
         });
     }
 
@@ -27,6 +29,8 @@ class AlterTableImportFieldsAddColumnClosedClaimDate extends Migration
     {
         Schema::table('import_fields', function (Blueprint $table) {
             $table->dropColumn('closed_claim_date');
+            $table->dropColumn('billed_submit_date');
+            $table->dropColumn('denial_code');
         });
     }
 }
